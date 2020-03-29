8f182705884016d8f826d73244ff4f131449015d
KradasA4 <wichai.sw19@gmail.com>
2020-03-29 17:35:40 +0700

    Updates

---
+++ precache-manifest.e599ec045e60ccbf7b86ddd443e2cb4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce90bc925954b1c0257ea088efe0fcf",
+    "url": "//kradasa4.github.io//card-album/index.html"
+  },
+  {
+    "revision": "38d3235f66e8dc98b0c7",
+    "url": "//kradasa4.github.io//card-album/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "9d8f24404b3e73e65043",
+    "url": "//kradasa4.github.io//card-album/static/css/main.1df0abb1.chunk.css"
+  },
+  {
+    "revision": "38d3235f66e8dc98b0c7",
+    "url": "//kradasa4.github.io//card-album/static/js/2.f5da23ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//kradasa4.github.io//card-album/static/js/2.f5da23ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8f24404b3e73e65043",
+    "url": "//kradasa4.github.io//card-album/static/js/main.ccf90b9d.chunk.js"
+  },
+  {
+    "revision": "5c5007433f708736a08e",
+    "url": "//kradasa4.github.io//card-album/static/js/runtime-main.a1bb096f.js"
+  }
+]);
